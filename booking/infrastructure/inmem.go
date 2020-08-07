f0aa1e6b7abf04e33ae796f765d8824d06e2d4bd
hageshtrem <hageshtrem@yahoo.com>
2020-08-07 22:24:38 +0300

    Inmem implementation of LocationRepository

--- booking/infrastructure/inmem.go
+++ booking/infrastructure/inmem.go
@@ -50,3 +50,41 @@ func NewCargoRepository() domain.CargoRepository {
 		cargos: make(map[domain.TrackingID]*domain.Cargo),
 	}
 }
+
+type locationRepository struct {
+	locations map[domain.UNLocode]*domain.Location
+}
+
+func (r *locationRepository) Find(locode domain.UNLocode) (*domain.Location, error) {
+	if l, ok := r.locations[locode]; ok {
+		return l, nil
+	}
+	return nil, domain.ErrUnknownLocation
+}
+
+func (r *locationRepository) FindAll() []*domain.Location {
+	l := make([]*domain.Location, 0, len(r.locations))
+	for _, val := range r.locations {
+		l = append(l, val)
+	}
+	return l
+}
+
+// NewLocationRepository returns a new instance of a in-memory location repository.
+func NewLocationRepository() domain.LocationRepository {
+	r := &locationRepository{
+		locations: make(map[domain.UNLocode]*domain.Location),
+	}
+
+	r.locations[domain.SESTO] = domain.Stockholm
+	r.locations[domain.AUMEL] = domain.Melbourne
+	r.locations[domain.CNHKG] = domain.Hongkong
+	r.locations[domain.USNYC] = domain.NewYork
+	r.locations[domain.USCHI] = domain.Chicago
+	r.locations[domain.JNTKO] = domain.Tokyo
+	r.locations[domain.DEHAM] = domain.Hamburg
+	r.locations[domain.NLRTM] = domain.Rotterdam
+	r.locations[domain.FIHEL] = domain.Helsinki
+
+	return r
+}
